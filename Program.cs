0697a427c4eb3949fa36da0a9085f18147b65355
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:55:20 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace App18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData = Console.ReadLine();
+            double number = Convert.ToSingle(inputData);
+            
+            string message = string.Empty;
+            bool check = number % 3 == 0 && number % 5 != 0;
+            if (check)
+            {
+                message = "Fizz";
+            }
+            else if (number % 3 != 0 && number % 5 == 0)
+            {
+                message = "Buzz";
+            }
+            else if (number % 3 == 0 && number % 5 == 0)
+            {
+                message = "FizzBuzz";
+            }
+            else
+            {
+                message = number.ToString();
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
